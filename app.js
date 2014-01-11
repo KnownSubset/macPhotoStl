293dc31cf167f85354cfde0968efc0dfc5854a7d
Nathan Dauber <nathan@radialogica.com>
2014-01-11 08:36:11 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    format = require('util').format,
+    fs = require('fs'),
+    spawn = require('child_process').spawn;
+
+var serverUrl = "http://report.radialogica.com";
+
+var app = module.exports = express();
+
+app.use(express.bodyParser( { keepExtensions: true } ));
+
+// These directories contain static files, so if the HTML want to load these
+// just load it, no need to set up
+app.use("/flat-ui", express.static(__dirname + '/flat-ui'));
+app.use("/common-files", express.static(__dirname + '/common-files'));
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/js", express.static(__dirname + '/js'));
+
+// GET on root will return the dashboard
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
---
+++ vagrant_bootstrap.sh
@@ -0,0 +1,4 @@
+apt-get update
+apt-get install -y curl g++ npm
+rm -rf /var/www
+ln -fs /vagrant /var/www
